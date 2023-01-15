811de5e17b06217add071fe030c2945ee88b1500
karthikeyan <karthikeyan@testpress.in>
2023-01-15 12:44:09 +0530

    Create Lets_learn app

--- Learning_management_system/Learning_management_system/settings.py
+++ Learning_management_system/Learning_management_system/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "Lets_learn.apps.LetsLearnConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
